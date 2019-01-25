d7bdb16191f7d911d211c34a761291218d60571a
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-01-25 10:56:42 +0200

    import quotes class to details component and add input binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { DetailsComponent } from './details/details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
+    DetailsComponent,
    
   ],
   imports: [
---
+++ src/app/details/details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { Quotes } from '../quotes';
+
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+ 
+  @Input() quote:Quotes;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
