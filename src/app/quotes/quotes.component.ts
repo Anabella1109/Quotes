e79442604235b8718d099b9421a57351db9c8fbd
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-01-29 10:29:30 +0200

    add font-awesome classes to html

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -13,7 +13,10 @@ export class QuotesComponent implements OnInit {
     new Quotes("Be yourself; everyone else is already takens", "Oscar Wilde",0,0,"D~babes",new Date(2018,11,13)),
     new Quotes("So many books, so little time.", "Frank Zappa",0,0,"Sallo Sal",new Date(2018,11,13)),
     new Quotes("A room without books is like a body without a soul.", " Marcus Tullius Cicero",0,0,"Anny Bella",new Date(2018,11,13)),
-    new Quotes("You only live once, but if you do it right, once is enough.", "Mae West",0,0,"Sallo Sal",new Date(2018,11,13))
+    new Quotes("You only live once, but if you do it right, once is enough.", "Mae West",0,0,"Sallo Sal",new Date(2018,11,13)),
+    new Quotes("In three words I can sum up everything I've learned about life: it goes on.", "Robert Frost",0,0,"Sallo Sal",new Date(2018,11,13)),
+    new Quotes("Friendship ... is born at the moment when one man says to another :What! You too? I thought that no one but myself . . .", " C.S. Lewis, The Four Loves",0,0,"Sallo Sal",new Date(2018,11,18))
+
 
   ];
   @Output() isHighest= new EventEmitter<boolean>();
